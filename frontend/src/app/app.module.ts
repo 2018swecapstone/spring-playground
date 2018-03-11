e2c61ed93ed1c9672055efb11f0bc1acd0a33229
Kevin Trosinski <ktrosinski@g.harvard.edu>
2018-03-11 18:27:02 -0400

    First pass at Event Submission page/form.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
+import {AppComponent} from './app.component';
+import {EventComponent} from './event/event.component';
 
-import { AppComponent } from './app.component';
+import {LocationsService, UserTypesService} from './api';
+import {EventTypesService} from './api';
+import {UsersService} from './api';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        EventComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule
+    ],
+    providers: [
+        HttpClientModule,
+        LocationsService,
+        UserTypesService,
+        UsersService,
+        EventTypesService
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
